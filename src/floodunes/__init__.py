177235b1377224d4b6619e5a585cd7f8f047c826
Martin Nguyen <martinnguyen20494@gmail.com>
2024-06-17 10:55:34 +1200

    13th commit

--- src/floodunes/__init__.py
+++ src/floodunes/__init__.py
@@ -2,7 +2,12 @@
 resreg: Resampling strategies for regression in Python
 """
 from . import *
-# from dataPrep import *
-# from testluke import testing_imports
+from .dataPrep import *
+from .dataCollection import *
+from .bayesFuncClassification import *
+from .rasterArray import *
+from .bayesRun import *
+from .bayesFuncRegression import *
+from .bayesPrepClassification import *
 
 __version__ = '0.0.1'
--- src/floodunes/testluke.py
+++
@@ -1,3 +0,0 @@
-def testing_imports():
-    print("this works")
-    
